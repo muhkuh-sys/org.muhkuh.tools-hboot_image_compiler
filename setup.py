897eb20d3991d079a04ab576ecc05be96341a017
fwoermann <fwoermann@hilscher.com>
2021-11-11 12:03:19 +0100

    [code] update setup.py to fix a bug that did not pack examples and other xml files

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(name='hboot_image_compiler',
       description='A tool used to compile HBoot images',
       # long_description=open("README.rst").read(),
       license='',
-      packages=find_packages(exclude=['tests', 'mbs', 'templates', 'src', 'targets', 'site_scons', 'build', 'dist']),
+      # packages=find_packages(exclude=['tests', 'mbs', 'templates', 'src', 'targets', 'site_scons', 'build', 'dist']),
+      packages=find_packages(exclude=['tests']),
+      # include_package_data=True,
       package_data={'hil_nxt_hboot_image_compiler': [
           'patch_tables/*.xml',
           'templates/app/*.xml',
@@ -20,7 +22,7 @@ setup(name='hboot_image_compiler',
           'elf_compiler/arm-none-eabi-gcc/4.9.3/bin/arm-none-eabi-objcopy.exe'
       ]},
 
-      include_package_data=True,
+
       cmdclass=versioneer.get_cmdclass(),
       zip_safe=False,
       extras_require={
